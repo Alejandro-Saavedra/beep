9ebbee5a529641f5d595d28c8596f59bfe690d79
Alejandro Saavedra <saavedraalex21@gmail.com>
2023-08-13 18:16:06 -0700

    Saving after making changes to id

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,14 +1,11 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
-import { createServerSideHelpers } from "@trpc/react-query/server";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
-import superjson from "superjson";
 import { PageLayout } from "~/components/cardLayout";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
 import { PostView } from "~/components/postView";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 const ProfileFeed = (props: { userId: string }) => {
   const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
@@ -61,11 +58,13 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const helpers = createServerSideHelpers({
-    router: appRouter,
-    ctx: { prisma, userId: null },
-    transformer: superjson, // optional - adds superjson serialization
-  });
+  const helpers = generateSSGHelper();
+  // export const getStaticProps: GetStaticProps = async (context) => {
+  //   const helpers = createServerSideHelpers({
+  //     router: appRouter,
+  //     ctx: { prisma, userId: null },
+  //     transformer: superjson, // optional - adds superjson serialization
+  //   });
 
   const slug = context.params?.slug;
 
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,18 +1,47 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
+import { PageLayout } from "~/components/cardLayout";
+import { PostView } from "~/components/postView";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
-// export default function Home() {
-const SinglePostPage: NextPage = () => {
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+  if (!data) return <div>404</div>;
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - @${data.author.userName}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>Post View</div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const helpers = generateSSGHelper();
+  const id = context.params?.id;
+
+  if (typeof id !== "string") {
+    throw new Error("no slug");
+  }
+
+  await helpers.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+      id,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default SinglePostPage;
