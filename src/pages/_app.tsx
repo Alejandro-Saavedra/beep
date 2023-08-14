add314b6e4fc4a0f48d5b8e79ddec6246fb47007
Alejandro Saavedra <saavedraalex21@gmail.com>
2023-08-13 19:49:50 -0700

    Saving after minor changes to index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
-import type { AppProps } from "next/app";
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import Head from "next/head";
 
-const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Head>
